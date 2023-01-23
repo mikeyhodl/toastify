692abd079065c57952b46ff8c00eb4b3f4125303
Mike Owino <mike.owino40@yahoo.com>
2023-01-23 20:35:44 +0000

    ()

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-// import "./App.css";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Button from "react-bootstrap/Button";
-import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
 
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+  
+  function App(){
+    const notify = () =>
+    toast.success("🦄 Wow so easy!", {
+      position: "bottom-right",
+      autoClose: 600,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
 
-function App() {
-  const notify = () => toast("Wow so easy!");
-  return (
-    <>
-      <Button variant="success" onClick={notify}>
-        Notify!
-      </Button>{" "}
-      <ToastContainer />
-      <Button variant="primary" size="lg" active>
-        Primary button
-      </Button>{" "}
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    </>
-  );
-}
-
+    return (
+      <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+      </div>
+    );
+  }
 export default App;
